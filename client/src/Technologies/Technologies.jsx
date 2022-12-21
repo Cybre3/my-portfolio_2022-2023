ce45bd3ae0143f03fcdb0a9a8dd74e04883ea155
Cybre3 <cybre3@gmail.com>
2022-12-21 01:52:10 -0600

    Updated Technologies Page
    removed mandala image,
    update page background to space/galaxy,
    added responsive/on click tool box chest video,
    added steady rotating and counter rotating animation CSS,
    added sliding animation to tools from tool box,
    rest of layout fully created (technology explanation boxes - left & right, nav icons - bottom left, back to menu button - bottom right), not responsive or functional, yet.
    Glitch in mongoDB inner circle icon - after full rotation, icon flips upside down.

--- client/src/Technologies/Technologies.jsx
+++ client/src/Technologies/Technologies.jsx
@@ -1,26 +1,117 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import $ from 'jquery';
 import './technologies.css';
+import PicsOuterCircle from './PicsOuterCircle';
+import PicsInnerCircle from './PicsInnerCircle';
 
 function Technologies(props) {
+  const [show, setShow] = useState(false);
+  const vid = useRef(null);
+
+  $(document).ready(function () {
+    $('.techno-tools-chest').on('click', () => {
+      $('.techno-chest-video').trigger('play');
+    });
+  });
+
   return (
     <div id="technologies-container">
-      <img
-        src={require('../assets/Technologies/techno-mandala.png')}
-        alt=""
-        className="techno-outer-circle"
-      />
-      <div className="techno-inner-circle">
-        <img
-          src={require('../assets/Technologies/html-css-js-logo.png')}
+      <div className="tech-circle-container">
+        {/* <img
+          src={require('../assets/Technologies/techno-mandala.png')}
           alt=""
-          className="techno-basics-image"
+          className="techno-outer-circle-mandala"
+        /> */}
+        <div className="techno-inner-circle">
+          <img
+            className="techno-basics-image"
+            src={require('../assets/Technologies/html-css-js-logo.png')}
+            alt=""
+          />
+        </div>
+        <PicsOuterCircle />
+        <PicsInnerCircle />
+      </div>
+      <div className="techno-info-box right">
+        <h2>Technology Icon Title</h2>
+        <p>Some Mumbo jumbo about technology icon</p>
+      </div>
+      <div className="techno-info-box left">
+        <h2>Technology Icon Title</h2>
+        <p>Some Mumbo jumbo about technology icon</p>
+      </div>
+      <div className="tool-box" style={{ display: show ? 'flex' : 'none' }}>
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
+        />
+        <img
+          className="tool-box-logo"
+          src={require('../assets/Technologies/logos/mongodb-logo.png')}
+          alt=""
         />
       </div>
       <img
         src={require('../assets/Technologies/techno-tools-chest.png')}
         alt=""
         className="techno-tools-chest"
+        onClick={() => setShow(show === false ? true : false)}
       />
+      <video ref={vid} className="techno-chest-video" style={{ display: show ? 'block' : 'none' }}>
+        <source
+          src={require('../assets/Technologies/tools-chest-animation.mp4')}
+          type="video/mp4"
+        />
+      </video>
+      <div className="techno-nav-icons">
+        <div className="techno-nav-icon">1</div>
+        <div className="techno-nav-icon">2</div>
+        <div className="techno-nav-icon">3</div>
+        <div className="techno-nav-icon">4</div>
+        <div className="techno-nav-icon">5</div>
+      </div>
+      <div className="exit-to-menu-button">BACK TO MENU</div>
     </div>
   );
 }
