582af0c935e752f2dc8ea553e9d56a19f98a15cf
Cybre3 <cybre3@gmail.com>
2022-12-23 20:24:45 -0600

    Updated UI - Simple Projects
    Updated background to white background photo,
    updated heading, navbar and project preview box colors.

--- client/src/Projects/SimpleProjects/SimpleProjects.jsx
+++ client/src/Projects/SimpleProjects/SimpleProjects.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import $ from 'jquery';
 import './simpleProjects.css';
 
 function SimpleProjects(props) {
+  $(document).ready(function () {
+    $('.carousel__face').hover(
+      function () {
+        $('.simple-projects-title-description').show();
+      },
+      function () {
+        $('.simple-projects-title-description').hide();
+      }
+    );
+  });
+
   return (
     <div className="simple-projects-background">
-    <h1 className='projects-heading'>PROJECTS</h1>
-      <nav className='projects-navbar'>
+      <h1 className="projects-heading">PROJECTS</h1>
+      <nav className="projects-navbar">
         <NavLink to="/projects/simple-projects">SIMPLE</NavLink>
         <NavLink to="/projects/intermediate-projects">INTERMEDIATE</NavLink>
         <NavLink to="/projects/capstone-projects">CAPSTONE</NavLink>
       </nav>
-      <div className="project-preview">
-        
+      <div className="simple-projects-title-description">
+        <h2 className="simple-projects-title">TITLE OF PROJECT</h2>
+        <p className="simple-projects-desciption">Some mumbo jumbo about projects in view</p>
       </div>
+      <div className="project-preview"></div>
       <div className="simple-projects-container">
         <div className="simple-projects-carousel">
           <div className="carousel__face">
