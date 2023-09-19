a147ddca67c9acd566a6b1c3bc745fad345edb28
Cybre3 <cybre3@gmail.com>
2023-09-19 14:33:12 -0500

    Client UI: Added a  back to portfolio btn

--- client/src/MainMenu/MainMenu.jsx
+++ client/src/MainMenu/MainMenu.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import MenuSelectItem from './../common/MenuSelectItem';
 
 import './mainMenu.css';
+import { NavLink } from 'react-router-dom';
 
 const menuItems = [
   { path: '/projects', title: 'Projects' },
@@ -21,13 +22,14 @@ function MainMenu() {
       <div className="ml-48 flex h-full w-60 flex-col items-center bg-black text-red-500">
         <h1 className="mt-20 mb-24 flex flex-col items-center text-3xl font-bold">
           <span>Starrika</span> <span>McCloud</span>{' '}
-          <span className="whitespace-pre-wrap text-lg">Full  Stack  Developer</span>
+          <span className="whitespace-pre-wrap text-lg">Full Stack Developer</span>
         </h1>
         <div className="flex h-full w-full flex-col text-center  text-lg">
           {menuItems.map((item) => (
             <MenuSelectItem title={item.title} path={item.path} />
           ))}
         </div>
+        <a className='mb-24' href="https://starrika-mccloud-portfolio.netlify.app/">Back To Portfolio</a>
       </div>
     </div>
   );
